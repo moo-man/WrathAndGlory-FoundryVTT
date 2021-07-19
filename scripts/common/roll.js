de309fe33111bb1512fa21dc3bc6fb82098a74a0
thul <thul@users.noreply.github.com>
2021-07-19 17:33:32 +0200

    Interpreted the rules wrong, resilience is affected by AP but can not go lower than 1

--- scripts/common/roll.js
+++ scripts/common/roll.js
@@ -113,12 +113,12 @@ function _dealDamageToTarget(rollData) {
 
   let target = game.user.targets.values().next().value.actor;
 
-  let armour = _computeArmour(target) - rollData.weapon.ap.total;
-  if (0 > armour) {
-    armour = 0;
+  let resilence = (target.combat.resilence.total + _computeArmour(target)) - rollData.weapon.ap.total;
+  if (0 >= resilence) {
+    resilence = 1;
   }
 
-  let dmgTaken = rollData.result.damage.total - (target.combat.resilence.total + (armour));
+  let dmgTaken = rollData.result.damage.total - resilence;
   if (0 === dmgTaken) {
     target.update({
       "data.combat.shock.value" : target.combat.shock.value + 1,
