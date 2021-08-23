abc3b2f8902e78524bb8ce1460f512ce541ba830
Russell <russellthurman94@gmail.com>
2021-08-23 00:26:01 -0500

    Fix resilience, token bars fill in reverse

--- scripts/actor/actor.js
+++ scripts/actor/actor.js
@@ -119,7 +119,7 @@ export class WrathAndGloryActor extends Actor {
         this.combat.defense.total = this._setDefault(this.attributes.initiative.total - 1 + this.combat.defense.bonus, 1);
         this.combat.resolve.total = this._setDefault(this.attributes.willpower.total - 1 + this.combat.resolve.bonus, 1);
         this.combat.conviction.total = this._setDefault(this.attributes.willpower.total + this.combat.conviction.bonus, 1);
-        this.combat.resilence.total = this._setDefault(this.attributes.toughness.total + 1 + this.combat.resilence.bonus + this.combat.resilence.armor, 1);
+        this.combat.resilience.total = this._setDefault(this.attributes.toughness.total + 1 + this.combat.resilience.bonus + this.combat.resilience.armor, 1);
         this.combat.wounds.max = this._setDefault((this.advances.tier * 2) + this.attributes.toughness.total + this.combat.wounds.bonus, 1);
         this.combat.determination.total = this._setDefault(this.attributes.toughness.rating + this.combat.determination.bonus, 1);
         this.combat.shock.max = this._setDefault(this.attributes.willpower.rating + this.advances.tier + this.combat.shock.bonus, 1);
