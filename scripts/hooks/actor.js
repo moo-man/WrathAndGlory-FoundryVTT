63fe9642498ef5694b3b93b7e8361c01c4fae451
Russell <russellthurman94@gmail.com>
2022-01-03 15:08:42 -0600

    Fix dynamic effects being applied twice by different users

--- scripts/hooks/actor.js
+++ scripts/hooks/actor.js
@@ -1,21 +1,23 @@
 export default function() {
-    Hooks.on("updateActor", (actor, data) => {
-        if (actor.combat.wounds.value && actor.getFlag("wrath-and-glory", "autoWounded"))
+    Hooks.on("updateActor", (actor, data, options, userId) => {
+        if (userId == game.user.id)
         {
-            if (!actor.hasCondition("wounded"))
-                actor.addCondition("wounded")
+            if (actor.combat.wounds.value && actor.getFlag("wrath-and-glory", "autoWounded"))
+            {
+                if (!actor.hasCondition("wounded"))
+                    actor.addCondition("wounded")
+            }
+            else if (actor.hasCondition("wounded") && actor.getFlag("wrath-and-glory", "autoWounded"))
+                actor.removeCondition("wounded")
+    
+    
+            if (actor.combat.shock.value > actor.combat.shock.max && actor.getFlag("wrath-and-glory", "autoExhausted"))
+            {
+                if (!actor.hasCondition("exhausted"))
+                    actor.addCondition("exhausted")
+            }
+            else if (actor.hasCondition("exhausted") && actor.getFlag("wrath-and-glory", "autoExhausted"))
+                actor.removeCondition("exhausted")
         }
-        else if (actor.hasCondition("wounded") && actor.getFlag("wrath-and-glory", "autoWounded"))
-            actor.removeCondition("wounded")
-
-
-        if (actor.combat.shock.value > actor.combat.shock.max && actor.getFlag("wrath-and-glory", "autoExhausted"))
-        {
-            if (!actor.hasCondition("exhausted"))
-                actor.addCondition("exhausted")
-        }
-        else if (actor.hasCondition("exhausted") && actor.getFlag("wrath-and-glory", "autoExhausted"))
-            actor.removeCondition("exhausted")
-
     })
 }
