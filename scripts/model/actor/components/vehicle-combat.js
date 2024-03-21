dda8dea82726f818396c708edd9dd2b9ba9494c0
Russell <russellthurman94@gmail.com>
2024-03-20 21:02:35 -0500

    vehicle fixes and localization

--- scripts/model/actor/components/vehicle-combat.js
+++ scripts/model/actor/components/vehicle-combat.js
@@ -3,20 +3,21 @@ export class VehicleCombatModel extends foundry.abstract.DataModel {
     static defineSchema() {
         return {
             defence: new foundry.data.fields.SchemaField({
-                bonus: new foundry.data.fields.NumberField(),
+                bonus: new foundry.data.fields.NumberField({initial : 0}),
+                // total: new foundry.data.fields.NumberField({initial : 0})
             }),
             resilience: new foundry.data.fields.SchemaField({
-                bonus: new foundry.data.fields.NumberField(),
-                total: new foundry.data.fields.NumberField()
+                bonus: new foundry.data.fields.NumberField({initial : 0}),
+                total: new foundry.data.fields.NumberField({initial : 0})
             }),
             wounds: new foundry.data.fields.SchemaField({
-                value: new foundry.data.fields.NumberField(),
-                bonus: new foundry.data.fields.NumberField(),
-                max: new foundry.data.fields.NumberField()
+                value: new foundry.data.fields.NumberField({initial : 0}),
+                bonus: new foundry.data.fields.NumberField({initial : 0}),
+                max: new foundry.data.fields.NumberField({initial : 0})
             }),
-            size: new foundry.data.fields.StringField(),
-            speed: new foundry.data.fields.NumberField(),
-            fly: new foundry.data.fields.NumberField(),
+            size: new foundry.data.fields.StringField({initial : "large"}),
+            speed: new foundry.data.fields.NumberField({initial : 0}),
+            fly: new foundry.data.fields.NumberField({initial : 0}),
         }
     }
 
