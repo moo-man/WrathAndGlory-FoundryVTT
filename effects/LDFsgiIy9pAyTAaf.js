8e7d4cfb96e3624d135a552963c28c64e8021368
Russell <russellthurman94@gmail.com>
2025-01-24 17:35:57 -0600

    fix flagellant script

--- effects/LDFsgiIy9pAyTAaf.js
+++ effects/LDFsgiIy9pAyTAaf.js
@@ -1,5 +1,6 @@
 await this.actor.update({"system.combat.wounds.value" : this.actor.system.combat.wounds.value + this.actor.system.advances.tier})
 
-this.item.effects.getName("Flagellated").update({disabled: false})
+await this.item.effects.getName("Flagellated").update({disabled: false})
+await this.effect.update({"disabled" : true})
 
 this.script.message(`Self-inflicted ${this.actor.system.advances.tier} Wounds`)
