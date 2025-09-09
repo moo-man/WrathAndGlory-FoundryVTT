8be5eaff9490f575958b92030600a0a1d75a9f17
jesuisfrog <work.baglio@gmail.com>
2025-09-09 12:09:46 -0700

    Initialized SocketHandlers.register

--- scripts/hooks/ready.js
+++ scripts/hooks/ready.js
@@ -3,6 +3,7 @@ import FoundryOverrides from "../common/overrides.js"
 
 export default function() {
     Hooks.once("ready", () => {
+        SocketHandler.register();
         WnGThemeConfig.setTheme();
         game.counter.render({force: true})
 
