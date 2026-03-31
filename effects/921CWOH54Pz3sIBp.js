ca5e30e1a8226d2cf18ce8ada9d3172ea98c48e4
Russell <russellthurman94@gmail.com>
2026-03-30 23:04:57 -0500

    fix defender of humanity script close #387

--- effects/921CWOH54Pz3sIBp.js
+++ effects/921CWOH54Pz3sIBp.js
@@ -1,5 +1,6 @@
-if (args.isSuccess && args.targetTokens.map(i => i.actor).filter(i => i).some(a => a.system.mob?.value))
+if (args.result.isSuccess && args.targetTokens.map(i => i.actor).filter(i => i).some(a => a.system.mob?.value))
 {
-	args.result.success += this.actor.system.advances.rank
-	args.result.text[this.effect.id] = {label : this.effect.name, description : `Added ${this.actor.system.advances.rank} Icons`}
+	let rank = this.actor.system.advances?.rank || 1;
+	args.result.success += rank;
+	args.result.text[this.effect.id] = {label : this.effect.name, description : `Added ${rank} Icons`}
 }
